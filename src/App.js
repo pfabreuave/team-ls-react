b31853d3c4eebe8d9a2bcf07d9b3c629afd0faf9
pfabreuave <pfabreuave@gmail.com>
2021-09-14 10:05:49 -0300

    restauro repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import { Imprimir } from './components/Imprimir';
 
-function App() {
+/*
+            obtendo os valores do local storage
+*/
+const getDatafromLS=()=>{
+      const data = localStorage.getItem('teams');
+      if(data){
+          return JSON.parse(data);
+      }
+      else{
+          return []
+      }
+}
+var avatar = ''
+export const App = () => {
+
+  /*
+            main array of objects state || teams state || teams array of objects
+  */
+
+  const [teams, setTeams]=useState(getDatafromLS());
+
+  /*
+            input field states
+  */
+
+  const [usergh, setUsergh]=useState('');
+  const [email, setEmail]=useState('');
+  const [linkedin, setLinkedin]=useState('');
+  const [usercw, setUsercw]=useState('');
+  
+  
+  /*
+            form submit event 
+  */
+
+  const handleAddTeamSubmit=(e)=>{
+
+    e.preventDefault();
+
+  /*
+            Access github looking for the element's avatar
+  */
+   
+    var urlu = "https://api.github.com/users/" + usergh;
+		fetch(urlu, {method: 'GET'})
+    		.then(function(response) {
+        	if (!response.ok) {
+            	throw Error(response.statusText);
+        	}
+        	return response.json();
+    		})
+			.then(function(users) {
+        avatar = users.avatar_url
+			
+    /*
+            creating an object
+    */
+       
+    let team={
+              avatar,     
+              usergh,
+              email,
+              linkedin,
+              usercw  
+    }
+  
+    setTeams([...teams,team]);
+   
+    setUsergh('');
+    setEmail('');
+    setLinkedin('');
+    setUsercw('');
+  })
+
+  .catch(function(error) {
+    alert(usergh +' no existe, verifique el codigo de usuario github')
+   	
+    });
+  }
+
+  /*
+            delete team from Local storage 
+  */
+  const deleteTeam=(usergh)=>{
+        const filteredTeams=teams.filter((element,index)=>{
+              return element.usergh !== usergh
+        })
+        setTeams(filteredTeams);
+  }
+
+  /*
+            saving data to local storage
+  */
+
+  useEffect(()=>{
+      localStorage.setItem('teams',JSON.stringify(teams));
+  },[teams])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+        <h1>Participantes curso React JS</h1>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <div className='main'>
+            <div className='form-container'>
+              <form autoComplete="off" className='form-group' onSubmit={handleAddTeamSubmit}>
+           
+                <input  type='text' 
+                        placeholder='User Github' 
+                        className='form-control' 
+                        required
+                        onChange={(e)=>setUsergh(e.target.value)} 
+                        value={usergh}>
+                </input>
+                <br></br>
+            
+                <input  type='text' 
+                        placeholder='E-mail'  
+                        className='form-control' 
+                        required
+                        onChange={(e)=>setEmail(e.target.value)} 
+                        value={email}>
+                </input>
+                <br></br>
+            
+                <input  type='text' 
+                        placeholder='Linkedin'  
+                        className='form-control' 
+                        required
+                        onChange={(e)=>setLinkedin(e.target.value)} 
+                        value={linkedin}>
+                </input>
+                <br></br>
+            
+                <input  type='text' 
+                        placeholder='User Codewars'  
+                        className='form-control' 
+                        required
+                        onChange={(e)=>setUsercw(e.target.value)} 
+                        value={usercw}>
+                </input>
+                <br></br>
+
+                <button type='submit' className='btn btn-secondary'>
+                  Adicionar Participante
+                </button>
+              </form>
+            </div>
+            <div className='view-container'>
+              {teams.length>0&&
+              <>
+                  <div className='table-responsive'>
+                    <table className='table'>
+                      <thead>
+                        <tr>
+                          <th>Student</th>
+                          <th>User Github</th>
+                          <th>Correo electronico</th>
+                          <th>URL linkedin</th>
+                          <th>User Codewars</th>
+                          <th>Selec</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <Imprimir teams={teams} deleteTeam={deleteTeam}/>
+                      </tbody>
+                    </table>
+                  </div>
+                  <button className='btn btn-danger btn-md'
+                    onClick={()=>setTeams([])}>Exclua todos os itens em LOCAL STORAGE</button>
+              </>
+              }
+              {teams.length < 1 && <div className='alerta'>Área de armazenamento vazia</div>}
+            </div>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/Imprimir.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Icon} from 'react-icons-kit'
+import {trash} from 'react-icons-kit/feather/trash'
+
+export const Imprimir = ({teams,deleteTeam}) => { 
+    
+    return teams.map(team=>(
+        
+        <tr key={team.usergh}>
+            <td><img src = {team.avatar} alt='imagen Avatar'/></td>
+            <td>
+                 <a  href= {'https://pfabreuave.github.io/api-github-javascript/?user=' + team.usergh} target='_blank' rel='noreferrer'>{team.usergh}</a>                    
+            </td>
+            <td>{team.email}</td>
+            <td>
+                 <a  href='https://www.linkedin.com/in/pedro-abreu-7a958a5b/' target='_blank' rel='noreferrer'>{team.linkedin}</a>                    
+            </td>
+            <td>
+                 <a  href={'https://pfabreuave.github.io/API-Codewars-JS/?user=' + team.usercw} target='_blank' rel='noreferrer'>{team.usercw}</a>                    
+            </td>
+            <td className='delete-btn' onClick={()=>deleteTeam(team.usergh)}>
+                <Icon icon={trash}/>
+            </td> 
+        </tr> 
+        
+        
+
+
+    
+))
+}
